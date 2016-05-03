e774789afbe4947c29ef734c404db9a66f9a8d0b
leesper <pascal7718@gmail.com>
2016-05-03 13:40:48 +0800

    add ConcurrentMap data structure

--- concurrent.go
+++ concurrent.go
@@ -1,6 +1,8 @@
 package tao
 
 import (
+  "errors"
+  "sync"
   "sync/atomic"
 )
 
@@ -55,4 +57,176 @@ func (a *AtomicBoolean) CompareAndSet(oldValue, newValue bool) bool {
     n = 0
   }
   return atomic.CompareAndSwapInt32((*int32)(a), o, n)
+}
+
+const INITIAL_SHARD_SIZE = 64
+var ErrorNilKey = errors.New("Nil key")
+var ErrorNilValue = errors.New("Nil value")
+
+type ConcurrentMap struct {
+  shards []*syncMap
+}
+
+func NewConcurrentMap() *ConcurrentMap {
+  return &ConcurrentMap{
+    shards: make([]*syncMap, INITIAL_SHARD_SIZE),
+  }
+}
+
+func (cm *ConcurrentMap)Put(k, v interface{}) error {
+  if isNil(k) {
+    return ErrorNilKey
+  }
+  if isNil(v) {
+    return ErrorNilValue
+  }
+  if shard, err := cm.shardFor(k); err != nil {
+    return err
+  } else {
+    shard.put(k, v)
+  }
+
+  return nil
+}
+
+func (cm *ConcurrentMap)Get(k interface{}) (interface{}, bool) {
+  if isNil(k) {
+    return nil, false
+  }
+  if shard, err := cm.shardFor(k); err != nil {
+    return nil, false
+  } else {
+    return shard.get(k)
+  }
+
+}
+
+func (cm *ConcurrentMap)Remove(k interface{}) bool {
+  if isNil(k) {
+    return false
+  }
+  if shard, err := cm.shardFor(k); err != nil {
+    return false
+  } else {
+    return shard.remove(k)
+  }
+}
+
+func (cm *ConcurrentMap)IsEmpty() bool {
+  return cm.Size() <= 0
+}
+
+func (cm *ConcurrentMap)Clear() {
+  for _, s := range cm.shards {
+    s.clear()
+  }
+}
+
+func (cm *ConcurrentMap)Size() int32 {
+  var size int32 = 0
+  for _, s := range cm.shards {
+    size += s.size()
+  }
+  return size
+}
+
+func (cm *ConcurrentMap)shardFor(k interface{}) (*syncMap, error) {
+  if code, err := hashCode(k); err != nil {
+    return nil, err
+  } else {
+    return cm.shards[code&uint32(INITIAL_SHARD_SIZE-1)], nil
+  }
+}
+
+func (cm *ConcurrentMap)IterKeys() <-chan interface{} {
+  kch := make(chan interface{})
+  go func() {
+    for _, s := range cm.shards {
+      s.RLock()
+      defer s.RUnlock()
+      for k, _ := range s.shard {
+        kch <- k
+      }
+    }
+    close(kch)
+  }()
+  return kch
+}
+
+func (cm *ConcurrentMap)IterValues() <-chan interface{} {
+  vch := make(chan interface{})
+  go func() {
+    for _, s := range cm.shards {
+      s.RLock()
+      defer s.RUnlock()
+      for _, v := range s.shard {
+        vch <- v
+      }
+    }
+    close(vch)
+  }()
+  return vch
+}
+
+func (cm *ConcurrentMap)IterItems() <-chan Item {
+  ich := make(chan Item)
+  go func() {
+    for _, s := range cm.shards {
+      s.RLock()
+      defer s.RUnlock()
+      for k, v := range s.shard {
+        ich <- Item{k, v}
+      }
+    }
+    close(ich)
+  }()
+  return ich
+}
+
+type Item struct {
+  Key, Value interface{}
+}
+
+type syncMap struct {
+  shard map[interface{}]interface{}
+  sync.RWMutex
+}
+
+func newSyncMap()*syncMap {
+  return &syncMap{
+    shard: make(map[interface{}]interface{}),
+  }
+}
+
+func (sm *syncMap) put(k, v interface{}) {
+  sm.Lock()
+  defer sm.Unlock()
+  sm.shard[k] = v
+}
+
+func (sm *syncMap) get(k interface{}) (interface{}, bool) {
+  sm.RLock()
+  defer sm.RUnlock()
+  v, ok := sm.shard[k]
+  return v, ok
+}
+
+func (sm *syncMap) size() int32 {
+  sm.RLock()
+  defer sm.RUnlock()
+  return int32(len(sm.shard))
+}
+
+func (sm *syncMap) remove(k interface{}) bool {
+  sm.Lock()
+  defer sm.Unlock()
+  _, ok := sm.shard[k]
+  delete(sm.shard, k)
+  return ok
+}
+
+func (sm *syncMap) clear() {
+  sm.Lock()
+  defer sm.Unlock()
+  sm.shard = make(map[interface{}]interface{})
 }
---
+++ util.go
@@ -0,0 +1,61 @@
+package tao
+
+import (
+  "hash/fnv"
+  "errors"
+  "reflect"
+  "bytes"
+  "encoding/binary"
+)
+
+type Hashable interface {
+	HashCode() int32
+}
+
+var ErrorNotHashable = errors.New("Not hashable")
+
+func hashCode(k interface{}) (uint32, error) {
+  h := fnv.New32a()
+  var code uint32
+  var err error
+  buf := new(bytes.Buffer)
+  switch v := k.(type) {
+  case bool:
+    if v {
+      binary.Write(buf, binary.BigEndian, uint8(1))
+      h.Write(buf.Bytes())
+    } else {
+      binary.Write(buf, binary.BigEndian, uint8(0))
+      h.Write(buf.Bytes())
+    }
+    code = h.Sum32()
+  case int, int8, int16, int32, int64, uint, uint8, uint16, uint32, uint64:
+    binary.Write(buf, binary.BigEndian, v)
+    h.Write(buf.Bytes())
+    code = h.Sum32()
+  case string:
+    h.Write([]byte(v))
+    code = h.Sum32()
+  case Hashable:
+    binary.Write(buf, binary.BigEndian, v.HashCode())
+    h.Write(buf.Bytes())
+    code = h.Sum32()
+  default:
+    err = ErrorNotHashable
+  }
+  return code, err
+}
+
+func isNil(v interface{}) bool {
+  if v == nil {
+    return true
+  }
+  rv := reflect.ValueOf(v)
+  kd := rv.Type().Kind()
+  switch kd {
+  case reflect.Ptr, reflect.Chan, reflect.Func, reflect.Interface, reflect.Map, reflect.Slice:
+    return rv.IsNil()
+  default:
+    return false
+  }
+}
