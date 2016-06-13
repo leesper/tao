e6d8aac05b6082c8cae12528b0a19806395843cd
leesper <pascal7718@gmail.com>
2016-06-13 15:01:22 +0800

    add supported TLS cipher suite

--- server.go
+++ server.go
@@ -329,6 +329,25 @@ func LoadTLSConfig(certFile, keyFile string, isSkipVerify bool) (tls.Config, err
   config = tls.Config{
     Certificates: []tls.Certificate{cert},
     InsecureSkipVerify: isSkipVerify,
+    CipherSuites: []uint16{
+      tls.TLS_RSA_WITH_RC4_128_SHA,
+      tls.TLS_RSA_WITH_3DES_EDE_CBC_SHA,
+      tls.TLS_RSA_WITH_AES_128_CBC_SHA,
+      tls.TLS_RSA_WITH_AES_256_CBC_SHA,
+      tls.TLS_RSA_WITH_AES_128_GCM_SHA256,
+      tls.TLS_RSA_WITH_AES_256_GCM_SHA384,
+      tls.TLS_ECDHE_ECDSA_WITH_RC4_128_SHA,
+      tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
+      tls.TLS_ECDHE_ECDSA_WITH_AES_256_CBC_SHA,
+      tls.TLS_ECDHE_RSA_WITH_RC4_128_SHA,
+      tls.TLS_ECDHE_RSA_WITH_3DES_EDE_CBC_SHA,
+      tls.TLS_ECDHE_RSA_WITH_AES_128_CBC_SHA,
+      tls.TLS_ECDHE_RSA_WITH_AES_256_CBC_SHA,
+      tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+      tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+      tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+      tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384, 
+    },
   }
   now := time.Now()
   config.Time = func() time.Time { return now }
