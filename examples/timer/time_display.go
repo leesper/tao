1f4ee4694e6316ecf534062159126f97e6b7eae2
leesper <pascal7718@gmail.com>
2016-06-29 16:22:53 +0800

    update time_display.go

--- examples/timer/time_display.go
+++ examples/timer/time_display.go
@@ -12,15 +12,9 @@ func main() {
   wheel := tao.NewTimingWheel()
   timerId := wheel.AddTimer(
     time.Now().Add(2 * time.Second),
-    500 * time.Millisecond,
+    1 * time.Second,
     tao.NewOnTimeOut(nil, func(t time.Time, d interface{}) { fmt.Printf("TIME OUT AT %s\n", t) }))
   fmt.Printf("Add timer %d\n", timerId)
-
-  timerId = wheel.AddTimer(
-    time.Now().Add(3 * time.Second),
-    50 * time.Millisecond,
-    tao.NewOnTimeOut(nil, func(t time.Time, d interface{}) { fmt.Printf("CANCEL ME IF YOU CAN\n") }))
-  fmt.Printf("Add another timer %d, now we have %d timers\n", timerId, wheel.Size())
 
   wg.Add(1)
   go func() {
@@ -34,19 +28,5 @@ func main() {
   }()
   wg.Wait()
 
-  wheel.CancelTimer(timerId)
-  fmt.Printf("Cancel timer %d, now we have %d timers\n", timerId, wheel.Size())
-
-  wg.Add(1)
-  go func() {
-    for i := 0; i < 10; i++ {
-      select {
-      case timeout := <-wheel.GetTimeOutChannel():
-        timeout.Callback(time.Now(), nil)
-      }
-    }
-    wg.Done()
-  }()
-  wg.Wait()
   wheel.Stop()
 }
