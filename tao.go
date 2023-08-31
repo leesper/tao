3640e3b295a1138a258f31b512fb48dd7a80b0f5
leesper <pascal7718@gmail.com>
2023-09-01 00:01:06 +0800

    bugfix: convert proto to lower case; proto other than tcp/udp/unix not supported

--- tao.go
+++ tao.go
@@ -1,12 +1,42 @@
 package tao
 
 import (
+	"fmt"
 	"strconv"
 	"strings"
 )
 
-func parseAddress(address string) (string, int, error) {
+var (
+	supportedProtos = map[string]bool{
+		"tcp":  true,
+		"udp":  true,
+		"unix": true,
+	}
+)
+
+func isSupportedProto(proto string) bool {
+	_, ok := supportedProtos[proto]
+	return ok
+}
+
+func parseAddress(address string) (proto string, port int, err error) {
 	parts := strings.Split(address, ":")
-	port, err := strconv.Atoi(parts[1])
-	return parts[0], port, err
+	proto = strings.ToLower(parts[0])
+	if !isSupportedProto(proto) {
+		return proto, port, newErrUnsupportedProto(proto)
+	}
+	port, err = strconv.Atoi(parts[1])
+	return proto, port, err
+}
+
+type ErrUnsupportedProto struct {
+	proto string
+}
+
+func (e ErrUnsupportedProto) Error() string {
+	return fmt.Sprintf("unsupported protocol %s", e.proto)
+}
+
+func newErrUnsupportedProto(proto string) ErrUnsupportedProto {
+	return ErrUnsupportedProto{proto: proto}
 }
--- tao_test.go
+++ tao_test.go
@@ -1,6 +1,7 @@
 package tao
 
 import (
+	"strings"
 	"testing"
 )
 
@@ -22,6 +23,29 @@ func TestShouldReturnErrorWhenPortNotNumber(t *testing.T) {
 	_, _, err := parseAddress("tcp:abcd")
 	if err == nil {
 		t.Fatalf("expected error, got nil")
+	}
+}
+
+func TestShouldReturnErrorIfProtoNotTcpUdpUnix(t *testing.T) {
+	_, _, err := parseAddress("abc:1234")
+	if err == nil {
+		t.Fatalf("expected error, got nil")
+	}
+	if !strings.Contains(err.Error(), "abc") {
+		t.Fatalf("unsupported protocol expected in error message")
+	}
+}
+
+func TestShouldParseAddressWhenProtoMixCased(t *testing.T) {
+	proto, port, err := parseAddress("TcP:8341")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if proto != "tcp" {
+		t.Fatalf("expected tcp, got %s", proto)
+	}
+	if port != 8341 {
+		t.Fatalf("expected 8341, got %d", port)
 	}
 }
 
